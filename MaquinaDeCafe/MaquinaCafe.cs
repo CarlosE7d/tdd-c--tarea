50d6b63abd1e28e5459b75aa038ef4d21dd3560d
CarlosE7d <202010782@itla.edu.do>
2023-04-09 15:13:54 -0400

    Maquina de cafe primera prueba lista

---
+++ MaquinaDeCafe/MaquinaCafe.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquinaDeCafe
+{
+    public class MaquinaCafe
+    {
+        public Cafetera? cafetera;
+        public Vaso vasoPequeno;
+        public Vaso vasoMediano;
+        public Vaso vasoGrande;
+        public Azucarero? azucar;
+        public void setVasosPequeno(Vaso vasoPequeno)
+        {
+            this.vasoPequeno = vasoPequeno;
+        }
+
+        public void setVasosMeadiano(Vaso vasoMediano)
+        {
+            this.vasoMediano = vasoMediano;
+        }
+
+        public void setVasosGrande(Vaso vasoGrande)
+        {
+            this.vasoGrande = vasoGrande;
+        }
+
+        public void setAzucarero(Azucarero azucarero)
+        {
+            this.azucar = azucarero;
+        }
+
+        public void setCafetera(Cafetera cafetera)
+        {
+            this.cafetera = cafetera;
+        }
+
+        public Vaso getTipoDeVaso(string tipoV)
+        {
+            if (tipoV.Equals("pequeno"))
+            {
+                return this.vasoPequeno;
+            }else if (tipoV.Equals("mediano"))
+            {
+                return this.vasoMediano;
+            }
+            else
+            {
+                return this.vasoGrande;
+            }
+        }
+    }
+}
---
+++ TestCafetera/TestMaquinaDeCafe.cs
@@ -0,0 +1,34 @@
+﻿using MaquinaDeCafe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework.Constraints;
+using Assert = NUnit.Framework.Assert;
+
+namespace TestCafetera
+{
+    public class TestMaquinaDeCafe
+    {
+        [Test]
+        public void debeDevolverUnVasoPequeno()
+        {
+            Cafetera cafetera = new Cafetera(50);
+            Vaso vasoPequeno = new Vaso(5, 10);
+            Vaso vasoMediano = new Vaso(5, 20);
+            Vaso vasoGrande = new Vaso(5, 30);
+            Azucarero azucarero = new Azucarero(20);
+
+
+            MaquinaCafe maquinaDeCafe = new MaquinaCafe();
+
+            maquinaDeCafe.setCafetera(cafetera);
+            maquinaDeCafe.setVasosPequeno(vasoPequeno);
+            maquinaDeCafe.setVasosMeadiano(vasoMediano);
+            maquinaDeCafe.setVasosGrande(vasoGrande);
+            maquinaDeCafe.setAzucarero(azucarero);
+
+            Vaso vaso = maquinaDeCafe.getTipoDeVaso("pequeno");
+
+            Assert.AreEqual(maquinaDeCafe.vasoPequeno, vaso);
+        }
+
+    }
+}
