6d00b635b771e141607706dadad2d2bf689068b5
CarlosE7d <202010782@itla.edu.do>
2023-04-09 18:14:30 -0400

    Maquina de cafe Listo

--- MaquinaDeCafe/Dispensador.cs
+++ MaquinaDeCafe/Dispensador.cs
@@ -19,9 +19,9 @@ namespace MaquinaDeCafe
         public Dispensador()
         {
             cafetera = new Cafetera(50);
-            vasoPequeno = new Vaso(5, 10);
-            vasoMediano = new Vaso(5, 20);
-            vasoGrande = new Vaso(5, 30);
+            vasoPequeno = new Vaso(10, 10);
+            vasoMediano = new Vaso(10, 20);
+            vasoGrande = new Vaso(10, 30);
             azucarero = new Azucarero(20);
             maquinaDeCafe = new MaquinaCafe();
             maquinaDeCafe.setCafetera(cafetera);
@@ -38,49 +38,74 @@ namespace MaquinaDeCafe
             try
             {
                 Console.Clear();
-                Console.WriteLine("---Seleccione el vaso que desea---\n1-Vaso Pequeño\n2-Vaso Meadiano\n3-Vaso Grande");
+                Console.WriteLine("---Seleccione el vaso que desea---\n1-Vaso Pequeño\n2-Vaso Meadiano\n3-Vas Grande\n4-Salir");
                 int opc = Convert.ToInt32(Console.ReadLine());
-                Vaso vaso = null;
-
+                Vaso vaso = null;                
                 switch (opc)
                 {
                     case (int)1:                        
                         vaso = maquinaDeCafe.getTipoDeVaso("pequeno");
+                        proceso(opc, vaso);
                         break;
 
                     case (int)2:
-                        vaso = maquinaDeCafe.getTipoDeVaso("mediano");                        
+                        vaso = maquinaDeCafe.getTipoDeVaso("mediano");
+                        proceso(opc, vaso);
                         break;
                     case (int)3:
                         vaso = maquinaDeCafe.getTipoDeVaso("mediano");
+                        proceso(opc, vaso);
                         break;
-                }
-
-                Console.WriteLine("---Seleccione la cantidad de vasos que desea---");
-                int cantVasos = Convert.ToInt32(Console.ReadLine());
-                Console.WriteLine("---Seleccione la cantidad de azucar que desea---");
-                int cantAzucar = Convert.ToInt32(Console.ReadLine());
-
-
-               string resultado = maquinaDeCafe.getVasoDeCafe(vaso, cantVasos, cantAzucar);
-                Console.Clear();
-                Console.WriteLine("****Su pedido fue****\n");
-                Console.WriteLine("---Tipo De Vaso---");
-                Console.WriteLine(vaso.ToString());
-                Console.WriteLine("---Cantidad De Vasos---");
-                Console.WriteLine(cantVasos);
-                Console.WriteLine("---Cantidad De Azucar---");
-                Console.WriteLine(cantAzucar);
-                Console.WriteLine(resultado);
-                Console.ReadLine();
-                menuPrincipal();
+                    case (int)4:
+                        break;
+                    default:
+                        Console.WriteLine("Opcion invalida");
+                        Console.ReadLine();
+                        menuPrincipal();
+                        break;
+                }               
             }
             catch
             {
                 Console.WriteLine("Opción invalida");
                 menuPrincipal();
             }
+        }
 
+        private void proceso(int opc, Vaso vaso)
+        {
+            string tipoDeVaso;
+            if (opc == 1)
+            {
+                tipoDeVaso = "   Vaso Pequeño";
+            }
+            else if (opc == 2)
+            {
+                tipoDeVaso = "   Vaso Mediano";
+            }
+            else
+            {
+                tipoDeVaso = "   Vaso Grande";
+            }
+
+            Console.WriteLine("---Seleccione la cantidad de vasos que desea---");
+            int cantVasos = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("---Seleccione la cantidad de azucar que desea---");
+            int cantAzucar = Convert.ToInt32(Console.ReadLine());
+
+
+            string resultado = maquinaDeCafe.getVasoDeCafe(vaso, cantVasos, cantAzucar);
+            Console.Clear();
+            Console.WriteLine("****Su pedido fue****\n");
+            Console.WriteLine("---Tipo De Vaso---");
+            Console.WriteLine(tipoDeVaso);
+            Console.WriteLine("---Cantidad De Vasos---");
+            Console.WriteLine(cantVasos);
+            Console.WriteLine("---Cantidad De Azucar---");
+            Console.WriteLine(cantAzucar);
+            Console.WriteLine(resultado);
+            Console.ReadLine();
+            menuPrincipal();
         }
     }
 }
