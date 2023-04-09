e6a699e3300cb8ef3d279e8a3adb1203dfdad89d
CarlosE7d <202010782@itla.edu.do>
2023-04-08 23:50:21 -0400

    HasAzucarListo

---
+++ MaquinaDeCafe/Azucarero.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaquinaDeCafe
+{
+    public class Azucarero
+    {
+        public int cantidadDeAzucar;
+        public Azucarero(int azucar)
+        {
+            this.cantidadDeAzucar = azucar;
+        }
+
+        public bool hasAzucar(int v)
+        {
+            if (cantidadDeAzucar >= v)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
---
+++ TestCafetera/TestAzucarero.cs
@@ -0,0 +1,22 @@
+﻿using MaquinaDeCafe;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Assert = NUnit.Framework.Assert;
+
+namespace TestCafetera{
+
+    
+    public class TestAzucarero
+    {
+
+        [Test]
+        public void deberiaDevolverVerdaderoSiHaySuficienteAzucarEnElAzucarero()
+        {
+            Azucarero azucarero = new Azucarero(10);
+
+            bool resultado = azucarero.hasAzucar(5);
+
+            Assert.IsTrue(resultado);
+        }
+
+    }
+}
